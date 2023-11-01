d16c84cd6766d5a662b3660c713c16a1149092cc
Matvey Smychkov <realsmych@gmail.com>
2023-11-01 17:42:52 +0000

    MessageRepo (#4)

--- lunatica-be/app/src/main/kotlin/uk/matvey/lunatica/message/Message.kt
+++ lunatica-be/app/src/main/kotlin/uk/matvey/lunatica/message/Message.kt
@@ -2,6 +2,7 @@ package uk.matvey.lunatica.message
 
 import java.time.Instant
 import java.util.UUID
+import java.util.UUID.randomUUID
 
 data class Message(
     val id: UUID,
@@ -9,4 +10,17 @@ data class Message(
     val content: String,
     val createdAt: Instant,
     val updatedAt: Instant
-)
+) {
+    companion object {
+        fun complaintMessage(complaintId: UUID, content: String): Message {
+            val now = Instant.now()
+            return Message(
+                randomUUID(),
+                complaintId,
+                content,
+                now,
+                now
+            )
+        }
+    }
+}
