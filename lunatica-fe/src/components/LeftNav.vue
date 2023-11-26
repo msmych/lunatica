cb64fcc5733423a648f6b1d1d2968047d3250166
Gleb Shipnaov <gcreego@gmail.com>
2023-11-26 11:14:51 +0100

    fix

--- lunatica-fe/src/components/LeftNav.vue
+++ lunatica-fe/src/components/LeftNav.vue
@@ -5,7 +5,7 @@
   import { useConfigStore } from './../state'
   import { UserType } from './../types/common.types'
 
-  import Gear from './../components/icons/Gear.svg'
+  // import Gear from './../components/icons/Gear.svg'
 
   const roles = Object.values(useConfigStore().user.roles)
 </script>
@@ -17,23 +17,23 @@
     </router-link>
     <ul>
       <li>
-        <Gear color="#fff" />
+        <!-- <Gear color="#fff" /> -->
         <router-link to="/complaint-new">Create new complaint</router-link>
       </li>
       <li>
-        <Gear color="#fff" />
+        <!-- <Gear color="#fff" /> -->
         <router-link to="/complaints">Active complaints</router-link>
       </li>
       <li v-if="roles.includes(UserType.Admin) || roles.includes(UserType.Worker)">
-        <Gear color="#fff" />
+        <!-- <Gear color="#fff" /> -->
         <router-link to="/complaints-completed">Completed complaints</router-link>
       </li>
       <li>
-        <Gear color="#fff" />
+        <!-- <Gear color="#fff" /> -->
         <router-link to="/settings">Settings</router-link>
       </li>
       <li v-if="roles.includes(UserType.Admin)">
-        <Gear color="#fff" />
+        <!-- <Gear color="#fff" /> -->
         <router-link to="/users">Users</router-link>
       </li>
     </ul>
